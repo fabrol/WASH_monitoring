334c6652130204fb2a92da61ebb82575ef3b68ea
Farhan Abrol <fabrol@fabrol-VirtualBox.(none)>
2012-06-21 13:49:18 +0300

    work dammit

--- kit_project/kit_src/kit/extensions/rapidsms/contact.py
+++ kit_project/kit_src/kit/extensions/rapidsms/contact.py
@@ -1,11 +1,10 @@
 from django.db import models
 from rapidsms.models import ContactBase
 class ExtendedContact(models.Model):
-
-	reporting_location = models.ForeignKey('locations.Location', blank=True, null=True)
-    def total_submissions(self):
-        from rapidsms_xforms.models import XFormSubmission
-        return XFormSubmission.objects.filter(connection__in=self.connection_set.all(), has_errors=False).count()
-
-    class Meta:
-        abstract = True
+	#reporting_location = models.ForeignKey('locations.Location', blank=True, null=True)
+	
+	def total_submissions(self):
+		from rapidsms_xforms.models import XFormSubmission
+		return XFormSubmission.objects.filter(connection__in=self.connection_set.all(), has_errors=False).count()
+	class Meta:
+		abstract = True
