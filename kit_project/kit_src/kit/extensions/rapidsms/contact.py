a642a28ed8eecd983296746755318c7d19d248bb
Farhan Abrol <fabrol@fabrol-VirtualBox.(none)>
2012-06-21 13:39:14 +0300

    removed mtrack from submodules

--- kit_project/kit_src/kit/extensions/rapidsms/contact.py
+++ kit_project/kit_src/kit/extensions/rapidsms/contact.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from rapidsms.models import ContactBase
 class ExtendedContact(models.Model):
 
+	reporting_location = models.ForeignKey('locations.Location', blank=True, null=True)
     def total_submissions(self):
         from rapidsms_xforms.models import XFormSubmission
         return XFormSubmission.objects.filter(connection__in=self.connection_set.all(), has_errors=False).count()
